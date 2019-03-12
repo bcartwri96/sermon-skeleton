12b1409be80d953e41f9a46e81d1880f0d9fd88c
Ben Cartwright <bcartwri96@gmail.com>
2019-03-13 10:08:13 +1100

    celery config Heroku test

--- src/celery_config.py
+++ src/celery_config.py
@@ -3,8 +3,10 @@ import configparser as cfg
 from src.scripts import index as sc
 
 import src.conf as cf
+bro = "redis://h:p33b23923fb4a76463b57ab4cb7ef9c04899975f22b3747b520db88255d41a4e1@ec2-3-209-60-144.compute-1.amazonaws.com:23849"
+back = "redis://h:p33b23923fb4a76463b57ab4cb7ef9c04899975f22b3747b520db88255d41a4e1@ec2-3-209-60-144.compute-1.amazonaws.com:23849"
 
 cel = Celery(cf.read_config('MAIN', 'APP_NAME'), \
-broker = cf.read_config('MAIN', 'CELERY_BROKER_URL'), \
-backend = cf.read_config('MAIN', 'CELERY_RESULT_BACKEND'), \
+broker = bro, \
+backend = back, \
 redis_max_connections = 10)
