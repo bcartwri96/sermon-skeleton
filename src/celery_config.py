87b917c9048027ced838615114aeffb427325f3d
Ben Cartwright <bcartwri96@gmail.com>
2019-03-13 10:40:55 +1100

    reshuffle files and add

--- src/celery_config.py
+++ src/celery_config.py
@@ -3,9 +3,12 @@ import configparser as cfg
 from src.scripts import index as sc
 
 import src.conf as cf
-bro = "redis://h:p33b23923fb4a76463b57ab4cb7ef9c04899975f22b3747b520db88255d41a4e1@ec2-3-209-60-144.compute-1.amazonaws.com:23849"
-back = "redis://h:p33b23923fb4a76463b57ab4cb7ef9c04899975f22b3747b520db88255d41a4e1@ec2-3-209-60-144.compute-1.amazonaws.com:23849"
-
+try:
+    bro = back = sc.get_env_variable('REDIS_URL')
+except Exception:
+    bro = cf.read_config('MAIN', 'CELERY_BROKER_URL')
+    back = cf.read_config('MAIN', 'CELERY_RESULT_BACKEND')
+    
 cel = Celery(cf.read_config('MAIN', 'APP_NAME'), \
 broker = bro, \
 backend = back, \
