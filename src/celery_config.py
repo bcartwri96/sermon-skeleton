fa890f6e3a3d8246eb083bb09321d0738b3c2224
Ben Cartwright <bcartwri96@gmail.com>
2019-03-12 19:08:30 +1100

    conf

--- src/celery_config.py
+++ src/celery_config.py
@@ -2,13 +2,9 @@ from celery import Celery
 import configparser as cfg
 from src.scripts import index as sc
 
-config = cfg.ConfigParser()
-config.read('config.ini')
+import src.conf as cf
 
-
-cel = Celery(config['MAIN']['APP_NAME'], \
-broker=config['MAIN']['CELERY_BROKER_URL'], \
-backend=config['MAIN']['CELERY_RESULT_BACKEND'], \
-redis_max_connections=10)
-
-cel.conf.update(config)
+cel = Celery(cf.read_config('MAIN', 'APP_NAME'), \
+broker = cf.read_config('MAIN', 'CELERY_BROKER_URL'), \
+backend = cf.read_config('MAIN', 'CELERY_RESULT_BACKEND'), \
+redis_max_connections = 10)
