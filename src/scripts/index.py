8a3fe38db0923b3c9beb867f287ef32885dfe8e4
Ben Cartwright <bcartwri96@gmail.com>
2019-05-13 14:34:45 +1000

    bug crush

--- src/scripts/index.py
+++ src/scripts/index.py
@@ -5,7 +5,7 @@ import os
 def get_env_variable(name):
     try:
         return os.environ[name]
-    except KeyError, Exception:
+    except KeyError:
         message = "Expected environment variable '{}' not set.".format(name)
         raise Exception(message)
 
